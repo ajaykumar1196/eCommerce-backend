f2b7ecfe8197d86da1412f8191f57066b3a65977
ajaykumar <ajaykumar1196@gmail.com>
2021-09-23 18:34:40 +0530

    added logging for inventory microservice

--- inventory-service/src/main/java/com/ecommerce/inventoryservice/controller/CartController.java
+++ inventory-service/src/main/java/com/ecommerce/inventoryservice/controller/CartController.java
@@ -3,12 +3,14 @@ package com.ecommerce.inventoryservice.controller;
 import com.ecommerce.inventoryservice.domain.Cart;
 import com.ecommerce.inventoryservice.domain.CartItem;
 import com.ecommerce.inventoryservice.service.CartService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
@@ -21,7 +23,7 @@ public class CartController {
 
     @GetMapping("/me")
     public ResponseEntity<Cart> getMyCart(@AuthenticationPrincipal Long userId){
-
+        log.info("Get user cart request from user {}", userId);
         Cart cart = cartService.findCartByUserId(userId);
 
         return new ResponseEntity<>(cart, HttpStatus.OK);
@@ -29,7 +31,7 @@ public class CartController {
 
     @PostMapping("/create")
     public ResponseEntity<Cart> createMyCart(@AuthenticationPrincipal Long userId){
-
+        log.info("Create cart request from user {}", userId);
         Cart cart = cartService.createCart(userId);
 
         return new ResponseEntity<>(cart, HttpStatus.CREATED);
@@ -38,6 +40,7 @@ public class CartController {
 
     @PostMapping("/add")
     public ResponseEntity<CartItem> addItemToCart(@AuthenticationPrincipal Long userId, @RequestBody CartItem cartItem){
+        log.info("Add item {} orders request from user {}", cartItem.getProduct().getName(), userId);
         CartItem newCartItem = cartService.addItemToCart(userId, cartItem);
         return new ResponseEntity<>(newCartItem, HttpStatus.CREATED);
     }
--- inventory-service/src/main/java/com/ecommerce/inventoryservice/controller/ProductController.java
+++ inventory-service/src/main/java/com/ecommerce/inventoryservice/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.ecommerce.inventoryservice.controller;
 
 import com.ecommerce.inventoryservice.domain.Product;
 import com.ecommerce.inventoryservice.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
@@ -25,7 +27,7 @@ public class ProductController {
 
     @GetMapping("/all")
     public ResponseEntity<List<Product>> findAllProductByCategory(@RequestParam("category") Long categoryId, Pageable pageable){
-
+        log.info("Get products request for a category : {}", categoryId);
         List<Product> products = productService.findAllByCategoryId(categoryId, pageable);
 
         return new ResponseEntity<>(products, HttpStatus.OK);
