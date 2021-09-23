d0ee3a324a2b70afcf51a22176ae5a36c335ce02
ajaykumar <ajaykumar1196@gmail.com>
2021-09-23 18:28:22 +0530

    added logging in InventoryEventSender and OrderEventSender

--- inventory-service/src/main/java/com/ecommerce/inventoryservice/messaging/OrderEventSender.java
+++ inventory-service/src/main/java/com/ecommerce/inventoryservice/messaging/OrderEventSender.java
@@ -3,12 +3,14 @@ package com.ecommerce.inventoryservice.messaging;
 import com.ecommerce.inventoryservice.dto.NewOrderEvent;
 import com.ecommerce.inventoryservice.dto.StockConfirmationEvent;
 import com.ecommerce.inventoryservice.dto.StockRejectionEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
+@Slf4j
 @Configuration
 public class OrderEventSender {
 
@@ -22,7 +24,7 @@ public class OrderEventSender {
     public static final String ORDER_STOCK_CONFIRM_TOPIC = "order.stock.confirm";
 
     public void sendStockReservationFailure(StockRejectionEvent stockRejectionEvent) {
-
+        log.info("Rejecting product stock reservation for order {} for user {}", stockRejectionEvent.getOrderId(), stockRejectionEvent.getUserId());
         String key = String.format("%s-stock-reservation", stockRejectionEvent.getOrderId());
         Message<StockRejectionEvent> event = MessageBuilder.withPayload(stockRejectionEvent).setHeader(KafkaHeaders.MESSAGE_KEY, key).build();
 
@@ -30,7 +32,7 @@ public class OrderEventSender {
     }
 
     public void sendStockReservationSuccess(StockConfirmationEvent stockConfirmationEvent) {
-
+        log.info("Rejecting product stock reservation for order {} for user {}", stockConfirmationEvent.getOrderId(), stockConfirmationEvent.getUserId());
         String key = String.format("%s-stock-release", stockConfirmationEvent.getOrderId());
         Message<StockConfirmationEvent> event = MessageBuilder.withPayload(stockConfirmationEvent).setHeader(KafkaHeaders.MESSAGE_KEY, key).build();
 
--- order-service/src/main/java/com/ecommerce/orderservice/messaging/InventoryEventSender.java
+++ order-service/src/main/java/com/ecommerce/orderservice/messaging/InventoryEventSender.java
@@ -2,12 +2,14 @@ package com.ecommerce.orderservice.messaging;
 
 import com.ecommerce.orderservice.domain.Order;
 import com.ecommerce.orderservice.dto.NewOrderEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
+@Slf4j
 @Configuration
 public class InventoryEventSender {
 
@@ -21,12 +23,14 @@ public class InventoryEventSender {
     public static final String INVENTORY_STOCK_RELEASE_TOPIC = "inventory.stock.release";
 
     public void sendProductStockReservationEvent(NewOrderEvent newOrderEvent) {
+        log.info("Reserving product stock for order {} by user {}", newOrderEvent.getOrderId(), newOrderEvent.getUserId());
         String key = String.format("%s-stock-reservation", newOrderEvent.getOrderId());
         Message<NewOrderEvent> event = MessageBuilder.withPayload(newOrderEvent).setHeader(KafkaHeaders.MESSAGE_KEY, key).build();
         streamBridge.send(INVENTORY_STOCK_RESERVE_TOPIC, event);
     }
 
     public void sendProductStockReleaseEvent(Order order) {
+        log.info("Releasing product stock for order {}", order.getId());
         String key = String.format("%s-stock-release", order.getId());
         Message<Order> event = MessageBuilder.withPayload(order).setHeader(KafkaHeaders.MESSAGE_KEY, key).build();
         streamBridge.send(INVENTORY_STOCK_RELEASE_TOPIC, event);
